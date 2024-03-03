c789038a4c517b43b51ee5417bc46d04ee6e7e10
André Freitas <a21112@alunos.ipca.pt>
2024-03-03 14:52:19 +0000

    Segunda fase feita

--- TPC3/geraTTL.py
+++ TPC3/geraTTL.py
@@ -1,13 +1,35 @@
 import json
 
 # Open the JSON file with UTF-8 encoding
-with open('dataset.json', 'r', encoding='utf-8') as f:
+with open('mapa.json', 'r', encoding='utf-8') as f:
     bd = json.load(f)
 
-ttl = """
-"""
+ttl = ""
 
-ttl += ttl
+for cidade in bd['cidades']:
+    registo = f"""
+###  http://rpcw.di.uminho.pt/2024/mapa#{cidade['id']}
+                                :{cidade['id']} rdf:type owl:NamedIndividual ,
+                                            :Cidade ;
+                                    :distrito "{cidade['distrito'].replace(" ", "_")}" ;
+                                    :idCidade "{cidade['id']}" ;
+                                    :nome "{cidade['nome'].replace(" ", "_")}" ;
+                                    :populacao {cidade['população']} .
+"""
+    ttl += registo
+
+for ligacao in bd['ligacoes']:
+    registo = f"""
+###  http://rpcw.di.uminho.pt/2024/mapa#{ligacao['id']}
+                                :{ligacao['id']} rdf:type owl:NamedIndividual ,
+                                            :Ligacao ;
+                                    :destinoLigacao :{ligacao['destino']} ;
+                                    :origemLigacao :{ligacao['origem']} ;
+                                    :distancia {ligacao['distância']} ;
+                                    :idLigacao "{ligacao['id']}" .
+"""
+    ttl += registo
+
 # Write the TTL data to a file
 with open('output.ttl', 'w', encoding='utf-8') as output_file:
     output_file.write(ttl)
